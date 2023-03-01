991cd90cfb4e498ad070f8043c590e0812b3644b
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2023-03-01 11:46:33 -0500

    FormGroup

--- src/app/admin/components/basic-form/basic-form.component.ts
+++ src/app/admin/components/basic-form/basic-form.component.ts
@@ -1,14 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-form',
   templateUrl: './basic-form.component.html',
   styleUrls: ['./basic-form.component.scss']
 })
+
 export class BasicFormComponent implements OnInit {
 
-  nameField = new FormControl('', [Validators.required, Validators.maxLength(10)]);
+  //FormGroup
+
+  form = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.maxLength(10)]),
+    email: new FormControl(''),
+    tel: new FormControl(''),
+    color: new FormControl('#000000'),
+    date: new FormControl(''),
+    age: new FormControl(12),
+    category: new FormControl(''),
+    multipleChecks: new FormControl(''),
+    agree: new FormControl(false),
+    gender: new FormControl(''),
+    zone: new FormControl(''),
+
+
+  })
+
+  /* nameField = new FormControl('', [Validators.required, Validators.maxLength(10)]);
   email = new FormControl('');
   telField = new FormControl('');
   colorField = new FormControl('');
@@ -22,31 +41,93 @@ export class BasicFormComponent implements OnInit {
 
   radioField = new FormControl('');
   zoneField = new FormControl('');
-  preferenciasField = new FormControl();
+  preferenciasField = new FormControl(); */
 
 
 
   constructor() { }
 
   ngOnInit(): void {
-    // escuchar cambios en el valor del campo
-    this.nameField.valueChanges.subscribe(value => {
-      console.log(value)
-    })
+
   }
 
   getNameValue() {
-    console.log(this.nameField.value)
-    if (1 >= 1) {
+    console.log(this.form.valid)
 
+  }
+
+  save() {
+    //Data form
+
+    console.log(this.form.value)
+  }
+
+
+
+
+  //Form Group
+
+  get nameFieldValid() {
+    return this.form.get('name').valid && this.form.get('name').touched
+  }
+  get nameFieldInvalid() {
+    return this.form.get('name').invalid && this.form.get('name').touched
+  }
+
+  get emailField() {
+    return this.form.get('email')
+  }
+
+  get telField() {
+    return this.form.get('tel')
+  }
+
+  get colorField() {
+    return this.form.get('color')
+  }
+
+  get dateField() {
+    return this.form.get('date')
+  }
+
+  get ageField() {
+    return this.form.get('age')
+  }
+
+  get categoryField() {
+    return this.form.get('category')
+  }
+
+  get multipleChecksField() {
+    return this.form.get('multipleChecks')
+  }
+
+  get agreeField() {
+    return this.form.get('agree')
+  }
+
+  get radioField() {
+    return this.form.get('gender')
+  }
+
+  get zoneField() {
+    return this.form.get('zone')
+  }
+
+
+
+
+
+
+
+
+  //Form Control
+  /* 
+    get isNameFieldValid() {
+      return this.nameField.valid && this.nameField.touched
     }
-  }
-
-  get isNameFieldValid() {
-   return  this.nameField.valid && this.nameField.touched
-  }
-  get isNameFieldInvalid() {
-    return this.nameField.invalid && this.nameField.touched
-  }
+    get isNameFieldInvalid() {
+      return this.nameField.invalid && this.nameField.touched
+    } */
 
 }
