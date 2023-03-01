dababa7905623085e713372de2fb7a6f8e3cd0b0
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2023-03-01 10:27:34 -0500

    Proyect uploaded

---
+++ src/app/admin/components/basic-form/basic-form.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-basic-form',
+  templateUrl: './basic-form.component.html',
+  styleUrls: ['./basic-form.component.scss']
+})
+export class BasicFormComponent implements OnInit {
+
+  nameField = new FormControl('', [Validators.required, Validators.maxLength(10)]);
+  email = new FormControl('');
+  telField = new FormControl('');
+  colorField = new FormControl('');
+  dateField = new FormControl('');
+  numberField = new FormControl('');
+
+  categoryField = new FormControl('value1');
+  multipleChecksField = new FormControl();
+
+  agreeField = new FormControl(false);
+
+  radioField = new FormControl('');
+  zoneField = new FormControl('');
+  preferenciasField = new FormControl();
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // escuchar cambios en el valor del campo
+    this.nameField.valueChanges.subscribe(value => {
+      console.log(value)
+    })
+  }
+
+  getNameValue() {
+    console.log(this.nameField.value)
+    if (1 >= 1) {
+
+    }
+  }
+
+}
