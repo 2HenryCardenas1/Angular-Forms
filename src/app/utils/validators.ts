8286128a4d730b525b4d2436a9b601ff5f10e0cf
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2023-03-01 23:00:25 -0500

    personal validations

--- src/app/utils/validators.ts
+++ src/app/utils/validators.ts
@@ -6,9 +6,29 @@ export class MyValidators {
     const value = control.value;
     console.log(value);
     if (value > 10000) {
-      return {price_invalid: true};
+      return { price_invalid: true };
     }
     return null;
   }
 
+
+  static validPassword(control: AbstractControl) {
+    // value of controller
+    const value = control.value;
+
+    if (!containNumbers(value)) {
+      return { password_invalid: true };
+    }
+    return null;
+  }
+
+}
+
+
+function containNumbers( value : string){
+  return value.split('').find( char => isNumber(char) !== undefined);
+}
+
+function isNumber(value: string) {
+  return !isNaN(parseInt(value, 10));
 }
