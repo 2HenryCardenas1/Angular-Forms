70850fa56e3613b25823b552767d8e6367fb1220
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2023-03-03 11:23:20 -0500

    form create category created

--- src/app/admin/categories/components/category-form/category-form.component.ts
+++ src/app/admin/categories/components/category-form/category-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-category-form',
   templateUrl: './category-form.component.html',
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryFormComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {
+    this.buildForm();
+  }
+
 
   ngOnInit(): void {
   }
+
+  private buildForm() {
+    this.form = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      image: ['', [Validators.required]],
+    });
+  }
+
+  get nameField() {
+    return this.form.get('name');
+  }
+
+  get imageField() {
+    return this.form.get('image');
+  }
+
+
 
 }
