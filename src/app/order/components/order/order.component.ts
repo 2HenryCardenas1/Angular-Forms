349cfd80a3261cdb83d773c50a004ed0c8529fea
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2023-03-04 18:09:40 -0500

    dinamic form created

--- src/app/order/components/order/order.component.ts
+++ src/app/order/components/order/order.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { Observable } from 'rxjs';
 
@@ -13,14 +14,52 @@ import { CartService } from './../../../core/services/cart.service';
 export class OrderComponent implements OnInit {
 
   products$: Observable<Product[]>;
+  form : FormGroup;
 
   constructor(
-    private cartService: CartService
+    private cartService: CartService,
+    private formBuilder: FormBuilder
   ) {
     this.products$ = this.cartService.cart$;
+    this.buildForm();
   }
 
   ngOnInit() {
+  }
+
+
+  private buildForm() {
+    this.form = this.formBuilder.group({
+      name : ['',[Validators.required]],
+      address : this.formBuilder.array([]) // Array of addresses
+    });
+  }
+
+
+  //create address form
+  addAddressField() {
+    this.addressField.push(this.createAddress());
+  }
+
+  private createAddress() {
+    return this.formBuilder.group({
+      zipCode : ['',[Validators.required]],
+      text : ['',[Validators.required]]
+    });
+  }
+
+
+  saveOrder() {
+    console.log(this.form.value)
+  }
+
+
+  get nameField() {
+    return this.form.get('name');
+  }
+
+  get addressField() {
+    return this.form.get('address') as FormArray;
   }
 
 }
